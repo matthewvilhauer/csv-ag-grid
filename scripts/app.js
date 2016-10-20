df8e4766f8d6e2b05001e9218fcc1ce78c3d0108
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-10-20 15:08:29 -0700

    Adds localStorage for storing the CSV file. Also adds a clear button to remove the saved CSV from localStorage and remove the associated grid.

--- scripts/app.js
+++ scripts/app.js
@@ -2,22 +2,76 @@ $( document ).ready(function() {
 
     var eGridDiv = document.querySelector('#ag-grid');
 
-    // The event listener for the file upload
+    // Event listeners for the file upload and clear data button
     document.getElementById('txtFileUpload').addEventListener('change', upload, false);
+    document.getElementById('clear-data').addEventListener('click', clearData, false);
+
+    if (localStorage.getItem("displaydata")) {
+        displayLocalData();
+    } else {
+        $("#clear-data").hide();
+    }
 
     // Method that checks that the browser supports the HTML5 File API
     function browserSupportFileUpload() {
         var isCompatible = false;
+
         if (window.File && window.FileReader && window.FileList && window.Blob) {
             isCompatible = true;
         }
         return isCompatible;
     }
 
-    function addAgGrid(data) {
+    // Method that reads and processes the selected file
+    function upload(evt) {
+        if (!browserSupportFileUpload()) {
+            alert('The File APIs are not fully supported in this browser!');
+        } else {
+            var data = null;
+            var file = evt.target.files[0];
+            var filename = evt.target.files[0].name;
+            var reader = new FileReader();
+            reader.readAsText(file);
 
-        $("#ag-grid-header").append('<h3>Upload Results</h3>');
+            reader.onload = function(event) {
+                var csvData = event.target.result;
+                data = $.csv.toArrays(csvData);
+                localStorage.setItem("displaydata", JSON.stringify(data));
+                localStorage.setItem("currentfile", filename);
+                displayLocalData();
+            };
+            reader.onerror = function() {
+                alert('Unable to read ' + file.fileName);
+            };
+        }
+    }
 
+    //Method for displaying an new ag-grid using the current localstorage data
+    function displayLocalData() {
+        var csvData = JSON.parse(localStorage.getItem("displaydata"));
+        var filename = localStorage.getItem("currentfile");
+        $("#clear-data").show();
+        $("#ag-grid-header").html('<h3>Upload Results for '+filename+'</h3>');
+        removeOldAgGrid();
+        addNewAgGrid(csvData);
+    }
+
+    //Method to clear the contents of the grid and its header
+    function removeOldAgGrid() {
+        $("#ag-grid-header").html('');
+        $("#ag-grid").html('');
+    }
+
+    //Method to clear localStorage and the file upload value, remove the ag-Grid, and hide the clear data button
+    function clearData() {
+        localStorage.removeItem("displaydata");
+        $("#txtFileUpload").val('');
+        $("#clear-data").hide();
+        removeOldAgGrid();
+    }
+
+    //Method to add an ag-Grid to the page
+    function addNewAgGrid(data) {
         var columnDefs = [];
         var rowData = [];
 
@@ -28,56 +82,20 @@ $( document ).ready(function() {
                 field: header.toLowerCase()
             });
         }
-
         for (var i = 1; i < data.length; i++) {
             var rowObject = {};
 
             for (var j = 0; j < data[i].length; j++) {
-                var rowObjectKey = data[0][j];
+                var rowObjectKey = data[0][j].toLowerCase();
                 rowObject[rowObjectKey] = data[i][j];
             }
             rowData.push(rowObject);
         }
-
         var gridOptions = {
             columnDefs: columnDefs,
             rowData: rowData
         };
-
         var eGridDiv = document.querySelector('#ag-grid');
         new agGrid.Grid(eGridDiv, gridOptions);
     }
-
-    // Method that reads and processes the selected file
-    function upload(evt) {
-        if (!browserSupportFileUpload()) {
-            alert('The File APIs are not fully supported in this browser!');
-        } else {
-            var data = null;
-            var file = evt.target.files[0];
-            var reader = new FileReader();
-            reader.readAsText(file);
-
-            reader.onload = function(event) {
-                var csvData = event.target.result;
-                data = $.csv.toArrays(csvData);
-
-                addAgGrid(data);
-
-                // //Append Results Header
-                // $("#results").append('<h3>CSV Data by Row</h3><table id="data-table"></table>');
-                // //Loop over each row
-                // for (var i = 0; i < data.length; i++) {
-                //     $("#data-table").append('<tr class="data-row data-row-'+i+'"></tr>');
-                //     for (var j = 0; j < data[i].length; j++) {
-                //         $(".data-row:last").append('<td class="data-cell data-cell-' + j + '">' + data[i][j] + '</td>');
-                //     }
-                // }
-            };
-            reader.onerror = function() {
-                alert('Unable to read ' + file.fileName);
-            };
-        }
-    }
-
 });
