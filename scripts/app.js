075a6aac24b9dc2745ce006f1c58cd19dd074695
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-10-23 21:55:03 -0700

    Files successfully saving to PouchDB

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,27 @@
 $( document ).ready(function() {
 
+    var db = new PouchDB('csv_db');
+
+    function getCSVFromLocalStorage(csvId) {
+        return db.get(csvId);
+    }
+    function putCSVIntoLocalStorage(filename, data) {
+        return db.put({
+            _id: new Date().toISOString(),
+            filename: filename,
+            data: data,
+            lasteditdate: formatDate(new Date())
+        });
+    }
+    function updateCSVIntoLocalStorage(csvId, filename, savedData) {
+        return db.put({
+            _id: csvId,
+            filename: filename,
+            data: savedData,
+            lasteditdate: formatDate(new Date())
+        });
+    }
+
     // Event listeners for the file upload and clear file list button
     document.getElementById('txtFileUpload').addEventListener('change', upload, false);
     document.getElementById('clear-uploads').addEventListener('click', clearUploads, false);
@@ -33,8 +55,8 @@ $( document ).ready(function() {
             reader.onload = function(event) {
                 var csvData = event.target.result;
                 data = $.csv.toArrays(csvData);
-                var createdDate = new Date();
-                saveUpload(filename, data, createdDate);
+                saveUpload(filename, data);
+                putCSVIntoLocalStorage(filename, data);
                 displayLocalData();
             };
             reader.onerror = function() {
@@ -44,12 +66,13 @@ $( document ).ready(function() {
     }
 
     //Method that adds an uploaded file to localStorage and sets the displaydata and currentfile to the uploaded file
-    function saveUpload(filename, data, timestamp) {
+    function saveUpload(filename, data) {
+        var timestamp = new Date().toISOString();
         var upload = {
-            key: filename+"_"+timestamp,
+            _id: timestamp,
             filename: filename,
             data: data,
-            lasteditdate: formatDate(timestamp)
+            lasteditdate: formatDate(new Date())
         };
         if (localStorage.getItem('uploads')) {
             var UploadList = JSON.parse(localStorage.getItem('uploads'));
@@ -57,10 +80,17 @@ $( document ).ready(function() {
             var UploadList = [];
         }
         UploadList.push(upload);
+        db.put(upload, function callback(err, result) {
+            console.log(result.id);
+            if (!err) {
+                console.log('Successfully saved a CSV');
+            }
+        });
+        var test = db.get(result.id);
         localStorage.setItem('uploads', JSON.stringify(UploadList));
         localStorage.setItem("displaydata", JSON.stringify(data));
         localStorage.setItem("currentfile", filename);
-        localStorage.setItem("currentkey", filename+"_"+timestamp);
+        localStorage.setItem("currentid", timestamp);
         renderFileList();
     }
 
@@ -89,7 +119,7 @@ $( document ).ready(function() {
         var displaySelectedData = function() {
             var key = this.id;
             for (var i = 0; i < files.length; i++) {
-                if (files[i].key == key) {
+                if (files[i]._id == key) {
                     var selectedData = JSON.stringify(files[i].data);
                     localStorage.setItem("currentfile", files[i].filename);
                     localStorage.setItem("displaydata", selectedData);
@@ -128,6 +158,7 @@ $( document ).ready(function() {
     function displayLocalData() {
         var csvData = JSON.parse(localStorage.getItem("displaydata"));
         var filename = localStorage.getItem("currentfile");
+        // var csvData = getCSVFromLocalStorage(csvId);
         removeOldAgGrid();
         addAgGrid(csvData);
     }
@@ -152,7 +183,7 @@ $( document ).ready(function() {
         var columnDefs = [];
         var rowData = [];
         var filename = localStorage.getItem("currentfile");
-        var filekey = localStorage.getItem("currentkey");
+        var file_id = localStorage.getItem("currentid");
         $("#ag-grid-header").html('<h4 class="grid-header">Upload Results for '+filename+'</h4>');
         for (var i = 0; i < data[0].length; i++) {
             var header = data[0][i];
@@ -195,7 +226,7 @@ $( document ).ready(function() {
             var files = JSON.parse(localStorage.getItem('uploads'));
 
             for (var i = 0; i < files.length; i++) {
-                if (files[i].key === filekey) {
+                if (files[i]._id === file_id) {
                     files[i].data = savedData;
                     files[i].lasteditdate = formatDate(new Date());
                     localStorage.setItem("currentfile", files[i].filename);
@@ -212,6 +243,7 @@ $( document ).ready(function() {
         new agGrid.Grid(eGridDiv, gridOptions);
         //Add the row data to the grid
         gridOptions.api.setRowData(rowData);
+        //Add save, download, and clear buttons to the grid header and attach their event listeners
         $("#ag-grid-header").append('<div class="grid-header" id="grid-buttons"><div id="save" class="btn btn-primary buttons"><span class="glyphicon glyphicon-floppy-disk"></span></div><div id="export" class="btn btn-success buttons"><span class="glyphicon glyphicon-download-alt"></span></div><div id="clear-data" class="btn btn-danger buttons"><span class="glyphicon glyphicon-remove"></span></div></div>');
         document.getElementById('clear-data').addEventListener('click', clearDisplayData, false);
         document.getElementById('export').addEventListener('click', onExport, false);
